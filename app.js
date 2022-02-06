536857db5d3bebf15372855705d15bbf87df0711
BanglaSky <bangla.sky.d1@gmail.com>
2022-02-06 13:39:46 +0000

    add a clock to the portfolio

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-$(document).ready(function(){ 
-     
-    $(".primaryContained").on('click', function(){
-    $(".comment").addClass("commentClicked");
-  });//end click
-  $("textarea").on('keyup.enter', function(){
-    $(".comment").addClass("commentClicked");
-  });//end keyup
-  });//End Function
- 
-new Vue({
-    el: "#app",
-    data:{
-       title: 'Add a comment',
-      newItem: '',
-      item: [],
-    },
-    methods:{
-      addItem  (){
-      this.item.push(this.newItem);
-        this.newItem = "";
-      }
-  }
- 
-  });
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
